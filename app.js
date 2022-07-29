25c5f1b82fc0febda146d5f05f51a9f2bc54962c
Achyut <aoogleak@gmail.com>
2022-07-29 20:55:51 +0545

    Added publish page to send mail and darkmode

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const { redirect } = require("express/lib/response");
+const { lte } = require("lodash");
+const nodemailer= require("nodemailer");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -35,6 +37,63 @@ app.get("/compose", function(req,res){
   res.render("compose");
 });
 
+app.get("/publish", function(req, res){
+  res.render("publish");
+});
+
+app.post("/publish", function(req,res){
+  const coming_post= {
+    fname: req.body.name,
+    email: req.body.email,
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  const output= `
+    <h1>Blog Website Research</h1>
+    <p>Details</p>
+    <ul>
+      <li>Full Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+    </ul>
+    <h2>Body</h2>
+    <p>${req.body.postBody}</p>
+  `;
+  //console.log(coming_post);
+
+  async function main(){
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp-mail.outlook.com",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: 'axyut@outlook.com',
+        pass: 'hotmailak001', 
+      },
+      tls:{
+        rejectUnauthorized:false
+      }
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: `${coming_post.fname} <axyut@outlook.com>`, // sender address
+      to: "aoogleak@gmail.com", // list of receivers
+      subject: coming_post.title, // Subject line
+      text: "Hello?", // plain text body
+      html: output, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    
+    
+  }
+  main().catch(console.error);
+  res.redirect('/');
+});
+
 app.post("/compose", function(req,res){
 
   const post = {
@@ -48,14 +107,12 @@ app.post("/compose", function(req,res){
 app.get("/posts/:postName", function(req,res){
  const reqTitle= _.lowerCase(req.params.postName);    // using lodash library to convert into lower case
 
- newposts.forEach(function(post){
-   const storedTitle = _.lowerCase(post.title);
+  newposts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
 
-   if ( storedTitle === reqTitle){
-     res.render("post", {title:post.title, content:post.content});
-   }else{
-    redirect("/");
-   }
+    if ( storedTitle === reqTitle){
+      res.render("post", {title:post.title, content:post.content});
+    }
  });
 
 });
