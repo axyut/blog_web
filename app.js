0799226d736a8ea7714ccbea67ba0919291f6762
Achyut <aoogleak@gmail.com>
2022-07-31 12:53:59 +0545

    4th gmail 0auth2 updated

--- app.js
+++ app.js
@@ -5,17 +5,31 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const { redirect } = require("express/lib/response");
-const { lte } = require("lodash");
+const { lte, result } = require("lodash");
+
+// Mailing ############
 const nodemailer= require("nodemailer");
+const { google } = require("googleapis");
+const Oauth2 = google.auth.OAuth2;
+const config = require("./config.js");
+
+const Redirect_URL = 'https://developers.google.com/oauthplayground'
+const clientId = config.clientId;
+const clientSecret= config.clientSecret;
+const refreshToken= config.refreshToken;
+
+const OAuth2Client = new google.auth.OAuth2(clientId, clientSecret, Redirect_URL )
+OAuth2Client.setCredentials( {refresh_token: refreshToken});
+
+
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
@@ -58,41 +72,51 @@ app.post("/publish", function(req,res){
     <h2>Body</h2>
     <p>${req.body.postBody}</p>
   `;
-  //console.log(coming_post);
 
-  async function main(){
+  async function sendMail(){
+    try {
+      const accessToken = await OAuth2Client.getAccessToken()
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      host: "smtp-mail.outlook.com",
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: 'axyut@outlook.com',
-        pass: 'hotmailak001', 
-      }, // for local host testing uncomment below
-      // tls:{
-      //   rejectUnauthorized:false
-      // }
-    });
+      const transport = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: '587',
+        secure: false,
+        service: 'mail',
+        auth:{
+          type: 'OAuth2',
+          user: 'kawuichan@gmail.com',
+          clientId: clientId,
+          clientSecret: clientSecret,
+          refreshToken: refreshToken,
+          accessToken: accessToken
+        }
+      })
 
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: `${coming_post.fname} <axyut@outlook.com>`, // sender address
-      to: "aoogleak@gmail.com", // list of receivers
-      subject: coming_post.title, // Subject line
-      text: "Hello?", // plain text body
-      html: output, // html body
-    });
+      const mailOptions = {
+        from: `${coming_post.fname} <kawuichan@gmail.com>`,
+        to: "aoogleak@gmail.com",
+        subject: coming_post.title,
+        text: "Hello?", 
+        html: output,
+      };
 
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    
-    
+      const result = await transport.sendMail(mailOptions)
+      return result
+      
+    } catch (error) {
+      return error
+    }
   }
-  main().catch(console.error);
+
+  sendMail().then(result => console.log('Email sent.', result))
+  .catch(error => console.log(error.message))
   res.redirect('/');
+
+
 });
+
+
+
 
 app.post("/compose", function(req,res){
 
