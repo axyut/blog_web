cccbc4103cc42ab99b1d52b0c234547fcdee3b64
Achyut <aoogleak@gmail.com>
2022-08-05 20:00:46 +0545

    6th search box css added

--- app.js
+++ app.js
@@ -8,11 +8,10 @@ const { redirect } = require("express/lib/response");
 const { lte, result } = require("lodash");
 const mongoose = require("mongoose");
 
-// Mailing ############
+// Mailing ##
 const nodemailer= require("nodemailer");
 const { google } = require("googleapis");
 const Oauth2 = google.auth.OAuth2;
-//const config = require("./config.js");
 
 const Redirect_URL = 'https://developers.google.com/oauthplayground'
 const clientId = '51365520624-m3eih0krlole7o6mfr0e6o9tioqoe1p5.apps.googleusercontent.com'
@@ -24,11 +23,9 @@ OAuth2Client.setCredentials( {refresh_token: refreshToken});
 
 const allContent = "Adios!";
 const blogContent = "Carry On with the reading.   ";
-const homeStartingContent = "Lacurra telrta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+app.use(express.json());
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -70,14 +67,8 @@ app.get("/all", function(req,res){
   res.render("all", {allContent: allContent });
 });
 
-
-
 app.get("/about", function(req,res){
-  res.render("about", {aContent: aboutContent});
-});
-
-app.get("/contact", function(req,res){
-  res.render("contact", {cContent: contactContent});
+  res.render("about", {aContent: allContent});
 });
 
 app.get("/compose", function(req,res){
