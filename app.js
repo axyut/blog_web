c0168d6be3f4ae0be45674037066627f7f97dbff
Achyut <aoogleak@gmail.com>
2022-08-24 13:44:40 +0545

    7.9 new fav icon added

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const _ = require("lodash");
 const { redirect } = require("express/lib/response");
 const { lte, result, parseInt } = require("lodash");
 const mongoose = require("mongoose");
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 
 // Mailing ##
 const nodemailer= require("nodemailer");
@@ -31,8 +31,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// favicon
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
+// // favicon
+// app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
 
 //let newposts=[];
