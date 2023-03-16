560d6d8045705d1c25ac2daf86ba5e7ef1e58606
Achyut <aoogleak@gmail.com>
2023-03-16 20:26:46 +0545

    9.10 change:environment

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const mongoURL = "mongodb://localhost:27017/blogDB";
-//const mongoURL = process.env.MONGO_CONNECT;
+const mongolocalURL = process.env.MONGO_LOCAL;
+const mongoURL = process.env.MONGO_CONNECT;
 
 module.exports = async () => {
 	try {
